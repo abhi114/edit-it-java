7d64d55da65d219d89bd8e4133565066fd5063c6
abhi114 <nabhi531@gmail.com>
2022-01-25 02:25:45 +0530

    added camera feature

--- app/src/main/java/com/example/edit_it/MainActivity.java
+++ app/src/main/java/com/example/edit_it/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.edit_it;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Toast;
 
@@ -12,10 +18,14 @@ import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity;
 import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants;
 import com.example.edit_it.databinding.ActivityMainBinding;
 
+import java.io.ByteArrayOutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     int IMAGE_REQUEST_CODE = 45;
+    int CAMERA_REQUEST_CODE  = 14;
+    int RESULT_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,21 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,IMAGE_REQUEST_CODE);
             }
         });
+
+        binding.cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},32);
+                }else if(ActivityCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},56);
+
+                }else{
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent,CAMERA_REQUEST_CODE);
+                }
+            }
+        });
     }
 
     @Override
@@ -47,16 +72,36 @@ public class MainActivity extends AppCompatActivity {
                 dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "Pico");
                 int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP};
                 dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
-                startActivityForResult(dsPhotoEditorIntent, 200);
+                startActivityForResult(dsPhotoEditorIntent, RESULT_CODE);
 
 
             }
         }
-        if(requestCode == 200){
+        if(requestCode == RESULT_CODE){
             Intent intent = new Intent(MainActivity.this,ResultActivity.class);
             intent.setData(data.getData());
             startActivity(intent);
 
         }
+        if(requestCode == CAMERA_REQUEST_CODE){
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            Uri uri = getImageUri(photo);
+            Intent dsPhotoEditorIntent = new Intent(this, DsPhotoEditorActivity.class);
+            dsPhotoEditorIntent.setData(uri);
+            dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "Pico");
+            int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP};
+            dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
+            startActivityForResult(dsPhotoEditorIntent, RESULT_CODE);
+
+
+        }
+    }
+
+    public Uri getImageUri(Bitmap bitmap){
+        ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,arrayOutputStream);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(),bitmap,"Title",null);
+        return Uri.parse(path);
+
     }
 }
